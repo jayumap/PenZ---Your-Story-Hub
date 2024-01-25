e47f3dc065b2e6de771f32a6096d9c3562f4d182
jayumap <umapjay4757@gmail.com>
2024-01-25 23:02:07 +0530

    registration page added

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PostItem from "./PostItem";
 import PostAuthor from "./PostAuthor";
+import { motion } from "framer-motion";
 
 import Thumbnail1 from "../images/blog20.jpg";
 import Thumbnail2 from "../images/blog100.jpg";
@@ -10,22 +11,39 @@ import Thumbnail4 from "../images/blog95.jpg";
 import { dummyPosts } from "../data";
 
 function Posts() {
+  const constraints = {
+    top: 0,
+    right: 0, // Adjust as needed
+    bottom: 0, // Adjust as needed
+    left: 0,
+  };
   const [posts, setPosts] = useState(dummyPosts);
   return (
     <section className="posts">
-      {posts.length > 0 ? <div className="container postsContainer">
-        {posts.map(({ id, thumbnail, category, title, desc, authorID }) => (
-          <PostItem
-            key={id}
-            postID={id}
-            thumbnail={thumbnail}
-            category={category}
-            title={title}
-            description={desc}
-            authorID={authorID}
-          />
-        ))}
-      </div> : <h2 className="center">No posts found</h2>}
+      {posts.length > 0 ? (
+        <div className="container postsContainer">
+          {posts.map(({ id, thumbnail, category, title, desc, authorID }) => (
+            <motion.div
+              drag
+              dragConstraints={constraints}
+              whileDrag={{ scale: 1.1 }}
+              dragElastic={0.1}
+            >
+              <PostItem
+                key={id}
+                postID={id}
+                thumbnail={thumbnail}
+                category={category}
+                title={title}
+                description={desc}
+                authorID={authorID}
+              />
+            </motion.div>
+          ))}
+        </div>
+      ) : (
+        <h2 className="center">No posts found</h2>
+      )}
     </section>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-export default Register
+  const changeInputHandler = (e) => {
+    setUserData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  return (
+    <section className="register">
+      <div className="container">
+        <h2>Sign Up</h2>
+        <form className="form registerForm">
+          <p className="formErrorMessage">This is an error message</p>
+          <input
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            value={userData.name}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="text"
+            placeholder="E-mail"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+          <button type="submit" className="btn primary">Register</button>
+        </form>
+        <small>Already have an account? <Link to= "/login">Sign In</Link></small>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
