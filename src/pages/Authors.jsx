c97135ced124b9a5a2f77b7350f0bc324378b266
jayumap <umapjay4757@gmail.com>
2024-01-30 23:16:54 +0530

    authors page updated

--- src/pages/Authors.jsx
+++ src/pages/Authors.jsx
@@ -4,7 +4,6 @@ import Avatar1 from "../images/avatar1.jpg";
 import Avatar2 from "../images/avatar2.jpg";
 import Avatar3 from "../images/avatar3.jpg";
 import Avatar4 from "../images/avatar4.jpg";
-import Avatar5 from "../images/avatar5.jpg";
 import { Link } from "react-router-dom";
 
 
