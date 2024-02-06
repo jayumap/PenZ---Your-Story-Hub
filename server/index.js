3d9c7f272a2606d9a5c322d17a93a07fc0083e04
jayumap <umapjay4757@gmail.com>
2024-02-06 15:34:55 +0530

    server setup done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const cors = require('cors')
-const {connect} = require('mongoose')
+const { connect } = require('mongoose')
 require('dotenv').config()
 
 const app = express();
 
-app.listen(5000, ()=>console.log("Server is running on port X000"))
+connect(process.env.MONGO_URI).then(app.listen(5000, () => console.log("Server is running on port 5000")))
+
